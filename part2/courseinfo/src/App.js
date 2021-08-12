158029cfc8ee92547492c9eef978ac3f0f4f7c34
Marcus Gugacs <m.gugacs@protonmail.com>
2021-08-12 15:00:30 +0200

    Exercise 2.5: separate module

--- part2/courseinfo/src/App.js
+++ part2/courseinfo/src/App.js
@@ -1,51 +1,9 @@
 import React from "react";
+import Course from "./components/Course"
 
 const Header = ({text}) => {
   return <h1>{text}</h1>
 };
-
-const Title = ({text}) => {
-  return <h2>{text}</h2>
-};
-
-const Part = ({part}) => {
-  return <p>{part.name} {part.exercises}</p>
-};
-
-const Content = ({parts}) => {
-  return (
-    <>
-      {
-        parts.map(part =>
-          <Part
-            key={part.id}
-            part={part}
-          />
-        )
-      }
-    </>
-  );
-};
-
-const Summary = ({parts}) => {
-  return (
-    <p>
-      <strong>
-         total of {parts.reduce((sum, part) => sum + part.exercises, 0)} exercises
-      </strong>
-    </p>
-  );
-};
-
-const Course = ({course}) => {
-  return (
-    <div>
-      <Title text={course.name} />
-      <Content parts={course.parts} />
-      <Summary parts={course.parts} />
-    </div>
-  );
-}
 
 const App = () => {
   const courses = [
@@ -90,7 +48,7 @@ const App = () => {
           id: 2
         }
       ]
-    },
+    }
   ];
 
   return (
