0ae15e5a15cbf3eae0e5a3ce469f126d4035f214
Marcus Gugacs <iimpaq@proton.me>
2021-08-12 08:57:57 +0200

    Exercise 1.10: unicafe step5
    Button was already extracted into its own component since the beginning. A separate StatisticLine component was added.

--- part1/unicafe/src/App.js
+++ part1/unicafe/src/App.js
@@ -12,6 +12,14 @@ const Button = ({handleClick, text}) => {
   );
 };
 
+const StatisticLine = ({text, value}) => {
+  return (
+    <p>
+      {text} {value}
+    </p>
+  );
+}
+
 const Statistics = ({good, neutral, bad}) => {
   const title = "statistics"
   const total = good + neutral + bad;
@@ -31,12 +39,12 @@ const Statistics = ({good, neutral, bad}) => {
   return (
     <>
       <h1>{title}</h1>
-      <p>good {good}</p>
-      <p>neutral {neutral}</p>
-      <p>bad {bad}</p>
-      <p>all {total}</p>
-      <p>average {average}</p>
-      <p>positive {positive}%</p>
+      <StatisticLine text="good" value={good} />
+      <StatisticLine text="neutral" value={neutral} />
+      <StatisticLine text="bad" value={bad} />
+      <StatisticLine text="all" value={total} />
+      <StatisticLine text="average" value={average} />
+      <StatisticLine text="positive" value={positive + "%"} />
     </>
   );
 }
