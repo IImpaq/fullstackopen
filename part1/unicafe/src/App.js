79525ca2809e375902026a700d72ed5b09167a5f
Marcus Gugacs <iimpaq@proton.me>
2021-08-12 08:44:58 +0200

    Exercise 1.7: unicafe step2

--- part1/unicafe/src/App.js
+++ part1/unicafe/src/App.js
@@ -12,13 +12,20 @@ const Button = ({handleClick, text}) => {
   );
 };
 
-const Statistics = (props) => {
+const Statistics = ({good, neutral, bad}) => {
+  const total = good + neutral + bad;
+  const average = (good - bad) / total;
+  const positive = good / total * 100;
+
   return (
     <>
       <h1>statistics</h1>
-      <p>good {props.good}</p>
-      <p>neutral {props.neutral}</p>
-      <p>bad {props.bad}</p>
+      <p>good {good}</p>
+      <p>neutral {neutral}</p>
+      <p>bad {bad}</p>
+      <p>all {total}</p>
+      <p>average {average}</p>
+      <p>positive {positive}%</p>
     </>
   );
 }
