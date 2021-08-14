1a8f8223896af4aa2608ed71bd86f3df5352560d
Marcus Gugacs <iimpaq@proton.me>
2021-08-14 12:59:24 +0200

    Exercise 2.12: data for countries step1

---
+++ part2/countries/src/App.js
@@ -0,0 +1,35 @@
+import React, {useState, useEffect} from "react"
+import axios from "axios"
+import Filter from "./components/Filter"
+import CountryList from "./components/CountryList"
+
+const App = () => {
+  const [countries, setCountries] = useState([]);
+  const [filter, setFilter] = useState("");
+
+  useEffect(() => {
+    axios
+      .get("https://restcountries.eu/rest/v2/all")
+      .then(response => {
+        setCountries(response.data);
+      })
+      .catch(error => {
+        console.log(error);
+      }); 
+  }, []);
+
+  console.log("rendering countries", countries.length);
+
+  const handleFilter = (event) => {
+    setFilter(event.target.value);
+  };
+
+  return (
+    <div>
+      <Filter value={filter} handleChange={handleFilter} />
+      <CountryList countries={countries} filter={filter} />
+    </div>
+  );
+};
+
+export default App;
