0c18fe02ed320d8f0e47695d73600cd05ffa6f92
Marcus Gugacs <m.gugacs@protonmail.com>
2021-08-18 15:54:16 +0200

    Exercise 3.11: phonebook backend step11

--- part3/deploy-full-phonebook.sh
+++ part3/deploy-full-phonebook.sh
@@ -5,21 +5,21 @@ read answer
 
 if [ "$answer" != "${answer#[Yy]}" ] ;then
   cd "${0%/*}"
-  echo "Current directory: $(pwd)"
+  echo "-> Current directory: $(pwd)"
   cd ./phonebook-frontend
-  echo "Moving to frontend directory: $(pwd)"
+  echo "-> Moving to frontend directory: $(pwd)"
   npm run build
-  echo "Built frontend website for deployment"
+  echo "-> Built frontend website for deployment"
   cp -r ./build ../phonebook-backend
-  echo "Copying build directory to backend $(pwd)/../phonebook-backend/"
+  echo "-> Copying build directory to backend $(pwd)/../phonebook-backend/"
   cd ../../
-  echo "Moving to root of repository: $(pwd)"
+  echo "-> Moving to root of repository: $(pwd)"
   git add .
-  echo "Adding changed files to "
+  echo "-> Adding changed files to "
   git commit -m "Deploying latest phonebok build"
-  echo "Committing latest build to git"
+  echo "-> Committing latest build to git"
   git subtree push --prefix part3/phonebook-backend heroku main
-  echo "Pushed subtree backend directory to heroku"
+  echo "-> Pushed subtree backend directory to heroku"
 else
-  echo "Exiting..."
+  echo "-> Exiting..."
 fi
--- part3/phonebook-backend/README.md
+++ part3/phonebook-backend/README.md
@@ -1,3 +1,13 @@
 # phonebook-backend
 
 Deployed using Heroku: https://protected-garden-15209.herokuapp.com
+
+## Notes
+
+The way of deploying was done a little bit differently compared to the
+theory of the course. Against the advise, the phonebook backend was kept
+within the same repository of the rest of the exercises. As already stated
+in theory, this made it harder/different to deploy using Heroku. To still
+be able to work within a single repository the git subtree command was used.
+I wrote a short deployment shell script (deploy-full-phonebook.sh), because 
+of the more complex way to deploy the website to the internet.
