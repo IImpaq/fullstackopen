8559ed37afb7b7559ed631a05b73435bc2c4974b
Marcus Gugacs <iimpaq@proton.me>
2021-08-18 20:44:41 +0200

    Exercise 3.13: phonebook database step1

--- part3/phonebook-backend/index.js
+++ part3/phonebook-backend/index.js
@@ -1,7 +1,10 @@
+require("dotenv").config();
+
 const express = require("express");
 const morgan = require("morgan");
 const cors = require("cors");
 const app = express();
+const Person = require("./models/person");
 
 app.use(cors());
 app.use(express.json());
@@ -11,103 +14,86 @@ morgan.token("person", (request, response) => JSON.stringify(request.body));
 app.use(morgan(":method :url :status :res[content-length] - :response-time ms :person"));
 
 let persons = [
-    { 
-      "id": 1,
-      "name": "Arto Hellas", 
-      "number": "040-123456"
-    },
-    { 
-      "id": 2,
-      "name": "Ada Lovelace", 
-      "number": "39-44-5323523"
-    },
-    { 
-      "id": 3,
-      "name": "Dan Abramov", 
-      "number": "12-43-234345"
-    },
-    { 
-      "id": 4,
-      "name": "Mary Poppendieck", 
-      "number": "39-23-6423122"
-    }
+  { 
+    "id": 1,
+    "name": "Arto Hellas", 
+    "number": "040-123456"
+  },
+  { 
+    "id": 2,
+    "name": "Ada Lovelace", 
+    "number": "39-44-5323523"
+  },
+  { 
+    "id": 3,
+    "name": "Dan Abramov", 
+    "number": "12-43-234345"
+  },
+  { 
+    "id": 4,
+    "name": "Mary Poppendieck", 
+    "number": "39-23-6423122"
+  }
 ]
 
-const randomID = (shouldVerify) => {
-    let id = Math.floor(Math.random() * 9999);
-    
-    if(!shouldVerify) {
-        return id;
-    } else {
-        let result = persons.find(person => person.id === id);
-        if(!result) {
-            return id;
-        } else {
-            return randomID(shouldVerify);
-        }
-    }
-}
-
 app.get("/info", (request, response) => {
-    let count = persons.length;
-    let time = new Date().toString();
-    response.send(`<p>Phonebook has info of ${count} people<br/>${time}</p>`);
+  let time = new Date().toString();
+  Person.find({}).then(result => {
+    response.send(`<p>Phonebook has info of ${result.length} people<br/>${time}</p>`);
+  });
 });
 
 app.get("/api/persons", (request, response) => {
-    response.json(persons);
+  Person.find({}).then(result => {
+    response.json(result);
+  });
 });
 
 app.get("/api/persons/:id", (request, response) => {
-    let id = Number(request.params.id);
-    let person = persons.find(person => person.id === id);
-
-    if(person) {
-        response.json(person);
-    } else {
-        response.status(404).end();
-    }
+  Person.findById(request.params.id).then(person => {
+    response.json(person);
+  });
 });
 
 app.post("/api/persons", (request, response) => {
-    let body = request.body;
+  let body = request.body;
 
-    if(!body.name || !body.number) {
-        return response.status(400).json({
-            error: "name or number missing"
-        });
-    }
+  if(!body.name || !body.number) {
+    return response.status(400).json({
+      error: "name or number missing"
+    });
+  }
 
-    if(persons.find(person => person.name === body.name)) {
-        return response.status(409).json({
-            error: "name must be unique"
-        });
-    }
+  if(persons.find(person => person.name === body.name)) {
+    return response.status(409).json({
+      error: "name must be unique"
+    });
+  }
 
-    let person = {
-        id: randomID(true),
-        name: body.name,
-        number: body.number
-    };
+  let person = {
+    id: randomID(true),
+    name: body.name,
+    number: body.number
+  };
 
-    persons = persons.concat(person);
+  persons = persons.concat(person);
 
-    response.json(person);
-})
-
-app.delete("/api/persons/:id", (request, response) => {
-    let id = Number(request.params.id);
-    let person = persons.find(person => person.id === id);
-
-    if(person) {
-        persons = persons.filter(person => person.id !== id);
-        response.json(person);
-    } else {
-        response.status(404).end();
-    }
+  response.json(person);
 });
 
-const PORT = process.env.PORT || 3001;
+app.delete("/api/persons/:id", (request, response) => {
+  let id = Number(request.params.id);
+  let person = persons.find(person => person.id === id);
+
+  if(person) {
+    persons = persons.filter(person => person.id !== id);
+    response.json(person);
+  } else {
+    response.status(404).end();
+  }
+});
+
+const PORT = process.env.PORT;
 app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`);
+  console.log(`Server running on port ${PORT}`);
 });
