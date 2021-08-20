8f769f9d38b1aa5205a7a4ab6830d4adb1183794
Marcus Gugacs <m.gugacs@protonmail.com>
2021-08-20 13:45:32 +0200

    Exercise 4.5: helper functions and unit tests step3

--- part4/bloglist/utils/list_helper.js
+++ part4/bloglist/utils/list_helper.js
@@ -4,7 +4,23 @@ const dummy = (blogs) => {
 };
 
 const totalLikes = (blogs) => {
-  return blogs.length === 0 ? 0 : blogs.reduce((acc, cur) => acc + cur.likes, 0);
+  return blogs.length === 0
+    ? 0
+    : blogs.reduce((acc, cur) => acc + cur.likes, 0);
 };
 
-module.exports = { dummy, totalLikes };
+const favoriteBlog = (blogs) => {
+  const result = blogs.length === 0
+    ? null
+    : blogs.reduce((last, curr) => curr.likes >= last.likes ? curr : last);
+
+  return result === null ? null : {
+    id: result._id,
+    title: result.title,
+    author: result.author,
+    url: result.url,
+    likes: result.likes
+  };
+};
+
+module.exports = { dummy, totalLikes, favoriteBlog };
