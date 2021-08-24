f5cebbb4971a0716ce3dfd2fec195476729d4acd
Marcus Gugacs <m.gugacs@protonmail.com>
2021-08-24 10:47:12 +0200

    Exercise 4.11: blog list tests step4

--- part4/bloglist/tests/blog_api.test.js
+++ part4/bloglist/tests/blog_api.test.js
@@ -21,10 +21,19 @@ describe("blog api tests", () => {
     };
 
     const old_blogs = await api.get("/api/blogs");
-    const result = await api.post("/api/blogs").send(samplePost);
+    const result = await api.post("/api/blogs").send(samplePost).expect(201);
     const new_blogs = await api.get("/api/blogs");
 
     expect(result.body).toStrictEqual({ ...samplePost, id: result.body.id });
     expect(new_blogs.body).toHaveLength(old_blogs.body.length + 1);
   });
+  test("that the likes property has a default value of 0", async () => {
+    const samplePost = {
+      title: "Jest Blog Post",
+      author: "Jest",
+      url: "http://jest.local/"
+    };
+    const result = await api.post("/api/blogs").send(samplePost).expect(201);
+    expect(result.body.likes).toEqual(0);
+  })
 });
