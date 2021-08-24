d8e650a1bd9a7926a695028a0db65f2d6893ac0e
Marcus Gugacs <iimpaq@proton.me>
2021-08-24 12:45:36 +0200

    Exercise 4.14: blog list expansions step2

--- part4/bloglist/controllers/blogs.js
+++ part4/bloglist/controllers/blogs.js
@@ -6,6 +6,17 @@ blogsRouter.get("/", async (request, response) => {
   response.json(result);
 });
 
+blogsRouter.get("/:id", async (request, response) => {
+  const id = request.params.id;
+  const blog = await Blog.findById(id);
+
+  if(blog) {
+    response.json(blog.toJSON());
+  } else {
+    response.status(404).end();
+  }
+});
+
 blogsRouter.post("/", async (request, response) => {
   const body = request.body;
   if(body.title === undefined && body.url === undefined) {
@@ -19,6 +30,24 @@ blogsRouter.post("/", async (request, response) => {
   const blog = new Blog(body);
   const result = await blog.save();
   response.status(201).json(result);
+});
+
+blogsRouter.put("/:id", async (request, response) => {
+  const id = request.params.id;
+  const body = request.body;
+  const newBlog = { };
+
+  if(body.author !== undefined) { newBlog.author = body.author; }
+  if(body.title !== undefined) { newBlog.title = body.title; }
+  if(body.url !== undefined) { newBlog.url = body.url; }
+  if(body.likes !== undefined) { newBlog.likes = body.likes; }
+
+  const result = await Blog.findByIdAndUpdate(id, newBlog, { new: true });
+  if(result) {
+    response.json(result.toJSON());
+  } else {
+    response.status(404).end();
+  }
 });
 
 blogsRouter.delete("/:id", async (request, response) => {
