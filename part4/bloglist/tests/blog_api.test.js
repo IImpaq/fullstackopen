7f0065f1a7a477c3dfe344df9baeb4dc4a6085f7
Marcus Gugacs <m.gugacs@protonmail.com>
2021-08-24 10:14:41 +0200

    Exercise 4.9: blog list tests step2

--- part4/bloglist/tests/blog_api.test.js
+++ part4/bloglist/tests/blog_api.test.js
@@ -3,9 +3,13 @@ const app = require("../app");
 const api = supertest(app);
 
 describe("blog api tests", () => {
-  test("number of blogs returned by api", async () => {
-    const result = await api.get("/api/blogs");
-
-    expect(result.body).toHaveLength(2);
+  test("the number of blogs returned by api", async () => {
+    const blogs = await api.get("/api/blogs");
+    expect(blogs.body).toHaveLength(2);
+  });
+  test("that the variable 'id' is defined", async () => {
+    const blogs = await api.get("/api/blogs");
+    if(blogs.body.length >= 1)
+      expect(blogs.body[0].id).toBeDefined();
   });
 });
