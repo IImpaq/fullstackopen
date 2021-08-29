64893025846fe01e0f2c22552e9832bf58cf889c
Marcus Gugacs <iimpaq@proton.me>
2021-08-29 13:22:38 +0200

    Exercise 4.23: blog list expansions step11

--- part4/bloglist/tests/blog_api.test.js
+++ part4/bloglist/tests/blog_api.test.js
@@ -5,9 +5,26 @@ const api = supertest(app);
 
 const helper = require("../utils/test_helper");
 const Blog = require("../models/blog");
+const User = require("../models/user");
 
+let user = null;
+let token = null;
 beforeAll(async () => {
   await Blog.deleteMany({});
+  await User.deleteMany({});
+
+  const username = "jest_username";
+  const password = "jest_password";
+  user = await api.post("/api/users/").send({
+    user: "jest_user",
+    username: username,
+    password: password
+  });
+  token = (await api.post("/api/login/").send({username, password })).body.token;
+
+  for(let i = 0; i < helper.initialBlogs.length; ++i) {
+    helper.initialBlogs[i].user = user.body.id;
+  }
   await Blog.insertMany(helper.initialBlogs);
 });
 
@@ -25,7 +42,7 @@ describe("getting blog posts", () => {
     const blogs = await api.get("/api/blogs");
     const firstBlogID = blogs.body[0].id;
     const blog = await api.get(`/api/blogs/${firstBlogID}`).expect(200);
-    expect(blog.body).toStrictEqual({ ...helper.initialBlogs[0], id: blog.body.id });
+    expect(blog.body).toStrictEqual({ ...helper.initialBlogs[0], id: blog.body.id, user: user.body.id });
   });
 });
 
@@ -38,10 +55,17 @@ describe("adding a blog post", () => {
       likes: 1
     };
     const oldBlogs = await api.get("/api/blogs");
-    const result = await api.post("/api/blogs").send(samplePost).expect(201);
+    const result = await api.post("/api/blogs")
+      .set("Authorization", `bearer ${token}`)
+      .send(samplePost)
+      .expect(201);
     const newBlogs = await api.get("/api/blogs");
 
-    expect(result.body).toStrictEqual({ ...samplePost, id: result.body.id });
+    expect(result.body).toStrictEqual({
+      ...samplePost,
+      id: result.body.id,
+      user: user.body.id
+    });
     expect(newBlogs.body).toHaveLength(oldBlogs.body.length + 1);
   });
   test("when the likes property is missing", async () => {
@@ -50,14 +74,29 @@ describe("adding a blog post", () => {
       author: "Jest",
       url: "http://jest.local/"
     };
-    const result = await api.post("/api/blogs").send(samplePost).expect(201);
+    const result = await api.post("/api/blogs")
+      .set("Authorization", `bearer ${token}`)
+      .send(samplePost)
+      .expect(201);
     expect(result.body.likes).toEqual(0);
   });
   test("when both the title and url property is missing", async () => {
     const samplePost = {
       author: "Jest"
     };
-    await api.post("/api/blogs").send(samplePost).expect(400);
+    await api.post("/api/blogs")
+      .set("Authorization", `bearer ${token}`)
+      .send(samplePost)
+      .expect(400);
+  });
+  test("when there is no authorization token provided", async () => {
+    const samplePost = {
+      title: "Jest Blog Post",
+      author: "Jest",
+      url: "http://jest.local/",
+      likes: 1
+    };
+    await api.post("/api/blogs").send(samplePost).expect(401);
   });
 });
 
@@ -72,9 +111,10 @@ describe("updating a blog post", () => {
       likes: 100
     };
     const result = await api.put(`/api/blogs/${firstBlogID}`)
+      .set("Authorization", `bearer ${token}`)
       .send(updatedBlog)
       .expect(200);
-    expect(result.body).toStrictEqual({ ...updatedBlog, id: result.body.id });
+    expect(result.body).toStrictEqual({ ...updatedBlog, id: result.body.id, user: user.body.id });
   });
   test("with a valid id and one property changed", async () => {
     const blogs = await api.get("/api/blogs");
@@ -83,6 +123,7 @@ describe("updating a blog post", () => {
       likes: 200
     };
     const result = await api.put(`/api/blogs/${firstBlogID}`)
+      .set("Authorization", `bearer ${token}`)
       .send(updatedBlog)
       .expect(200);
     expect(result.body).toStrictEqual({ ...result.body, likes: updatedBlog.likes });
@@ -92,7 +133,23 @@ describe("updating a blog post", () => {
     const updatedBlog = {
       likes: 200
     };
-    await api.put(`/api/blogs/${firstBlogID}`).send(updatedBlog).expect(404);
+    await api.put(`/api/blogs/${firstBlogID}`)
+      .set("Authorization", `bearer ${token}`)
+      .send(updatedBlog)
+      .expect(404);
+  });
+  test("when there is no authorization token provided", async () => {
+    const blogs = await api.get("/api/blogs");
+    const firstBlogID = blogs.body[0].id;
+    const updatedBlog = {
+      author: "Updated Author",
+      title: "Updated Title",
+      url: "http://updated-url.local",
+      likes: 100
+    };
+    await api.put(`/api/blogs/${firstBlogID}`)
+      .send(updatedBlog)
+      .expect(401);
   });
 });
 
@@ -100,7 +157,9 @@ describe("deleting a blog post", () => {
   test("with a valid id", async () => {
     const oldBlogs = await api.get("/api/blogs");
     const firstBlogID = oldBlogs.body[0].id;
-    await api.delete(`/api/blogs/${firstBlogID}`).expect(204);
+    await api.delete(`/api/blogs/${firstBlogID}`)
+      .set("Authorization", `bearer ${token}`)
+      .expect(204);
     const newBlogs = await api.get("/api/blogs");
     expect(newBlogs.body).toHaveLength(oldBlogs.body.length - 1);
   });
@@ -116,6 +175,12 @@ describe("deleting a blog post", () => {
     await api.delete("/api/blogs/").expect(404);
     const newBlogs = await api.get("/api/blogs");
     expect(newBlogs.body).toHaveLength(oldBlogs.body.length);
+  });
+  test("when there is no authorization token provided", async () => {
+    const oldBlogs = await api.get("/api/blogs");
+    const firstBlogID = oldBlogs.body[0].id;
+    await api.delete(`/api/blogs/${firstBlogID}`)
+      .expect(401);
   });
 });
 
