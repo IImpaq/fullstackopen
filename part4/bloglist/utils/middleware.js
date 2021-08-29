40984a29f7e5a5c357331a595ceb87fd4145ad59
Marcus Gugacs <m.gugacs@protonmail.com>
2021-08-29 11:05:35 +0200

    Exercise 4.19: blog list expansions step7

--- part4/bloglist/utils/middleware.js
+++ part4/bloglist/utils/middleware.js
@@ -5,14 +5,15 @@ const unknownEndpoint = (request, response) => {
 };
 
 const errorHandler = (error, request, response, next) => {
-  logger.error(error.message);
-
   switch(error.name) {
   case "CastError":
     return response.status(400).send({ error: "malformed id" });
   case "ValidationError":
     return response.status(400).json({ error: error.message });
+  case "JsonWebTokenError":
+    return response.status(401).json({ error: "invalid or missing token" });
   default:
+    logger.error(error.message);
     next(error);
   }
 };
