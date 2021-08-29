ab50c6cdcaf74eaeaad9b667f9fbeeede8fe30d9
Marcus Gugacs <m.gugacs@protonmail.com>
2021-08-29 10:52:21 +0200

    Exercise 4.18: blog list expansions step6

--- part4/bloglist/app.js
+++ part4/bloglist/app.js
@@ -3,6 +3,7 @@ const app = express();
 require("express-async-errors");
 const blogsRouter = require("./controllers/blogs");
 const usersRouter = require("./controllers/users");
+const loginRouter = require("./controllers/login");
 const config = require("./utils/config");
 const cors = require("cors");
 const logger = require("./utils/logger");
@@ -24,6 +25,7 @@ app.use(cors());
 app.use(express.json());
 app.use("/api/blogs", blogsRouter);
 app.use("/api/users", usersRouter);
+app.use("/api/login", loginRouter);
 
 app.use(middleware.unknownEndpoint);
 app.use(middleware.errorHandler);
