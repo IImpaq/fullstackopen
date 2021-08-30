5597843f2e87d34701f155adff1e427b9eab076c
Marcus Gugacs <iimpaq@proton.me>
2021-08-30 10:43:04 +0200

    Exercise 5.6: bloglist frontend step6

---
+++ part5/bloglist-frontend/src/components/BlogForm.js
@@ -0,0 +1,48 @@
+import React, { useState } from "react";
+
+const BlogForm = ({ createBlog }) => {
+  const [blogTitle, setBlogTitle] = useState("");
+  const [blogAuthor, setBlogAuthor] = useState("");
+  const [blogURL, setBlogURL] = useState("");
+
+  const handleBlogCreation = (event) => {
+    event.preventDefault();
+    createBlog({
+      title: blogTitle,
+      author: blogAuthor,
+      url: blogURL
+    });
+    setBlogTitle("");
+    setBlogAuthor("");
+    setBlogURL("");
+  }
+
+  return (
+    <>
+      <h2>create new</h2>
+      <form onSubmit={handleBlogCreation}>
+        <div>
+          title: <input value={blogTitle} name="blogTitle"
+                        onChange={({target}) =>
+                          setBlogTitle(target.value)
+                        } />
+        </div>
+        <div>
+          author: <input value={blogAuthor} name="blogAuthor"
+                         onChange={({target}) =>
+                           setBlogAuthor(target.value)
+                         } />
+        </div>
+        <div>
+          url: <input value={blogURL} name="blogURL"
+                      onChange={({target}) =>
+                        setBlogURL(target.value)
+                      } />
+        </div>
+        <button type="submit">create</button>
+      </form>
+    </>
+  );
+};
+
+export default BlogForm;
