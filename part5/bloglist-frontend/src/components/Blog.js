c3bbacb7aa5b63a7581a8059a2cf2dfba384bf78
Marcus Gugacs <m.gugacs@protonmail.com>
2021-08-30 10:55:00 +0200

    Exercise 5.7: bloglist frontend step7

--- part5/bloglist-frontend/src/components/Blog.js
+++ part5/bloglist-frontend/src/components/Blog.js
@@ -1,9 +1,36 @@
-import React from "react";
+import React, { useState } from "react";
 
-const Blog = ({blog}) => (
-  <div>
-    <strong>{blog.title}</strong> by {blog.author}
-  </div>
-);
+const Blog = ({blog}) => {
+  const [showDetails, setShowDetails] = useState(false);
+
+  const blogStyle = {
+    paddingTop: 10,
+    paddingLeft: 2,
+    border: 'solid',
+    borderWidth: 1,
+    marginBottom: 5
+  }
+
+  const visibleDetails = { display: showDetails ? "" : "none" };
+
+  const toggleDetails = (event) => {
+    event.preventDefault();
+    setShowDetails(!showDetails);
+  }
+
+  return (
+    <div style={blogStyle}>
+      <div>
+        <strong>{blog.title}</strong> by {blog.author}
+        <button onClick={toggleDetails}>{showDetails ? "hide" : "view"}</button>
+      </div>
+      <div style={visibleDetails}>
+        available at {blog.url}<br />
+        likes: {blog.likes}<br />
+        posted by: {blog.user.name}
+      </div>
+    </div>
+  );
+};
 
 export default Blog;
