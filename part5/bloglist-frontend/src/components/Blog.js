5e771dec43a03c3b4e83ebaea75d5272941feff4
Marcus Gugacs <iimpaq@proton.me>
2021-08-30 15:46:24 +0200

    Exercise 5.8: bloglist frontend step8

--- part5/bloglist-frontend/src/components/Blog.js
+++ part5/bloglist-frontend/src/components/Blog.js
@@ -1,22 +1,33 @@
 import React, { useState } from "react";
+import blogService from "../services/blogs";
 
-const Blog = ({blog}) => {
+const Blog = ({ blog, updateBlog }) => {
   const [showDetails, setShowDetails] = useState(false);
+  const [currentLikes, setCurrentLikes] = useState(blog.likes);
 
   const blogStyle = {
     paddingTop: 10,
     paddingLeft: 2,
-    border: 'solid',
+    border: "solid",
     borderWidth: 1,
     marginBottom: 5
-  }
+  };
 
   const visibleDetails = { display: showDetails ? "" : "none" };
 
   const toggleDetails = (event) => {
     event.preventDefault();
     setShowDetails(!showDetails);
-  }
+  };
+
+  const handleLike = (event) => {
+    event.preventDefault();
+    blog.likes = blog.likes + 1;
+    updateBlog(blog.id, {
+      likes: blog.likes
+    });
+    setCurrentLikes(blog.likes);
+  };
 
   return (
     <div style={blogStyle}>
@@ -26,7 +37,7 @@ const Blog = ({blog}) => {
       </div>
       <div style={visibleDetails}>
         available at {blog.url}<br />
-        likes: {blog.likes}<br />
+        likes: {currentLikes} <button onClick={handleLike}>like</button><br />
         posted by: {blog.user.name}
       </div>
     </div>
