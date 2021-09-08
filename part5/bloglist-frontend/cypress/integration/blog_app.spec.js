85a5d59b404a8870c1041116b9ef369dc1888e22
Marcus Gugacs <iimpaq@proton.me>
2021-09-08 10:31:11 +0200

    Exercise 5.20: bloglist end to end testing step4

--- part5/bloglist-frontend/cypress/integration/blog_app.spec.js
+++ part5/bloglist-frontend/cypress/integration/blog_app.spec.js
@@ -4,11 +4,27 @@ describe("Blog app", function() {
     username: "root_user",
     password: "root_password"
   };
+  const blogUrl = "http://localhost:3000";
+
+  function createBlog(title, author, url) {
+    cy.contains("create new blog").click();
+    cy.get("#inputTitle").type(title);
+    cy.get("#inputAuthor").type(author);
+    cy.get("#inputURL").type(url);
+    cy.get("#buttonCreate").click();
+    cy.contains(title);
+  }
+  function checkErrorNotification(text, color) {
+    cy.get(".error")
+      .should("contain", text);
+    cy.get(".error")
+      .should("have.css", "color", color);
+  }
 
   beforeEach(function() {
     cy.request("POST", "http://localhost:3003/api/testing/reset");
     cy.request("POST", "http://localhost:3003/api/users/", user);
-    cy.visit("http://localhost:3000");
+    cy.visit(blogUrl);
   });
   it("Login form is shown", function() {
     cy.contains("login").as("loginButton");
@@ -26,10 +42,7 @@ describe("Blog app", function() {
       cy.get("#username").type("root_user");
       cy.get("#password").type("wrong");
       cy.get("#login-button").click();
-      cy.get(".error")
-        .should("contain", "Invalid username or password");
-      cy.get(".error")
-        .should("have.css", "color", "rgb(255, 0, 0)");
+      checkErrorNotification("Invalid username or password", "rgb(255, 0, 0)");
     });
     it("succeeds with correct credentials", function() {
       cy.get("#username").type("root_user");
@@ -43,14 +56,19 @@ describe("Blog app", function() {
       cy.login({ username: user.username, password: user.password });
     });
     it("a new blog can be created", function() {
-      const title = "a test blog created by cypress";
-
-      cy.contains("create new blog").click();
-      cy.get("#inputTitle").type(title);
-      cy.get("#inputAuthor").type("cypress test");
-      cy.get("#inputURL").type("cypress-test-blog.local");
-      cy.get("#buttonCreate").click();
-      cy.contains(title);
+      createBlog(
+        "a test blog created by cypress",
+        "a test blog created by cypress",
+        "cypress-test-blog.local"
+      );
+    });
+    it("all users can like a blog", function() {
+      const title = "a popular test blog created by cypress";
+      createBlog(title,
+        "cypress test",
+        "popular-cypress-test-blog.local");
+      cy.contains("view").click();
+      cy.contains("like").click();
     });
   });
 });
--- part5/bloglist-frontend/cypress/support/commands.js
+++ part5/bloglist-frontend/cypress/support/commands.js
@@ -27,7 +27,7 @@
 Cypress.Commands.add("login", function({username, password}) {
   cy.request("POST", "http://localhost:3003/api/login", {
     username, password
-  }).then((response) => {
+  }).then(function(response) {
     localStorage.setItem("loggedInUser", JSON.stringify(response.body));
     cy.visit("http://localhost:3000");
   });
