b1fe74f5f67eb8c1838fb142a3121f3a7236be74
Marcus Gugacs <iimpaq@proton.me>
2021-09-09 11:32:03 +0200

    Exercise 6.15: Anecdotes and the backend step3

--- part6/redux-anecdotes/src/reducers/anecdoteReducer.js
+++ part6/redux-anecdotes/src/reducers/anecdoteReducer.js
@@ -1,3 +1,5 @@
+import anecdoteService from "../services/anecdotes";
+
 const reducer = (state = [], action) => {
   switch(action.type) {
   case "VOTE":
@@ -21,17 +23,23 @@ const reducer = (state = [], action) => {
   }
 };
 
-export const addAnecdote = (data) => {
-  return {
-    type: "ADD_ANECDOTE",
-    data
+export const addAnecdote = (content) => {
+  return async dispatch => {
+    const newAnecdote = await anecdoteService.createAnecdote(content);
+    dispatch({
+      type: "ADD_ANECDOTE",
+      data: newAnecdote
+    });
   };
 };
 
-export const initializeAnecdotes = (anecdotes) => {
-  return {
-    type: "INIT_ANECDOTES",
-    data: anecdotes
+export const initializeAnecdotes = () => {
+  return async dispatch => {
+    const anecdotes = await anecdoteService.getAll();
+    dispatch({
+      type: "INIT_ANECDOTES",
+      data: anecdotes
+    });
   };
 };
 
