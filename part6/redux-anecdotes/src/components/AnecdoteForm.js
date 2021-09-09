5b314646e669ed734132955a6715f910e3ca1b6c
Marcus Gugacs <iimpaq@proton.me>
2021-09-09 11:08:55 +0200

    Exercise 6.14: Anecdotes and the backend step2

--- part6/redux-anecdotes/src/components/AnecdoteForm.js
+++ part6/redux-anecdotes/src/components/AnecdoteForm.js
@@ -2,15 +2,17 @@ import React from "react";
 import { useDispatch } from "react-redux";
 import { addAnecdote } from "../reducers/anecdoteReducer";
 import { endNotification, notifyWith } from "../reducers/notificationReducer";
+import anecdoteService from "../services/anecdotes";
 
 const AnecdoteForm = () => {
   const dispatch = useDispatch();
 
-  const createAnecdote = (event) => {
+  const handleCreateAnecdote = async (event) => {
     event.preventDefault();
     const content = event.target.anecdote.value;
+    const newAnecdote = await anecdoteService.createAnecdote(content);
     event.target.anecdote.value = "";
-    dispatch(addAnecdote(content));
+    dispatch(addAnecdote(newAnecdote));
     dispatch(notifyWith("you created '" + content + "'"));
     setTimeout(() => dispatch(endNotification()), 5000);
   };
@@ -18,7 +20,7 @@ const AnecdoteForm = () => {
   return (
     <div>
       <h2>create new</h2>
-      <form onSubmit={createAnecdote}>
+      <form onSubmit={handleCreateAnecdote}>
         <div><input name="anecdote"/></div>
         <button type="submit">create</button>
       </form>
