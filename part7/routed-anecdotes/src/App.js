d1cf46e2e2847603eff8b9c6a5673f29a01668c9
Marcus Gugacs <iimpaq@proton.me>
2021-09-10 20:47:36 +0200

    Exercise 7.2: routed anecdotes step2

--- part7/routed-anecdotes/src/App.js
+++ part7/routed-anecdotes/src/App.js
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import { Link, Route, Switch } from "react-router-dom";
+import {Link, Route, Switch, useRouteMatch} from "react-router-dom";
 
 const Menu = () => {
   const padding = {
@@ -14,11 +14,23 @@ const Menu = () => {
   );
 };
 
+const Anecdote = ({ anecdote }) => {
+  return (
+    <div>
+      <h2>{anecdote.content} by {anecdote.author}</h2>
+      <p>has {anecdote.votes}</p>
+      <p>for more info see {anecdote.info}</p>
+    </div>
+  );
+};
+
 const AnecdoteList = ({ anecdotes }) => (
   <div>
     <h2>Anecdotes</h2>
     <ul>
-      {anecdotes.map(anecdote => <li key={anecdote.id} >{anecdote.content}</li>)}
+      {anecdotes.map(anecdote => <li key={anecdote.id}>
+        <Link to={`/anecdotes/${anecdote.id}`}>{anecdote.content}</Link>
+      </li>)}
     </ul>
   </div>
 );
@@ -107,7 +119,7 @@ const App = () => {
     setAnecdotes(anecdotes.concat(anecdote));
   };
 
-  //const anecdoteById = (id) => anecdotes.find(a => a.id === id);
+  const anecdoteById = (id) => anecdotes.find(a => a.id === id);
 
   /*const vote = (id) => {
     const anecdote = anecdoteById(id);
@@ -120,11 +132,19 @@ const App = () => {
     setAnecdotes(anecdotes.map(a => a.id === id ? voted : a));
   };*/
 
+  const match = useRouteMatch("/anecdotes/:id");
+  const anecdote = match
+    ? anecdoteById(match.params.id)
+    : null;
+
   return (
     <div>
       <h1>Software anecdotes</h1>
       <Menu />
       <Switch>
+        <Route path="/anecdotes/:id">
+          <Anecdote anecdote={anecdote} />
+        </Route>
         <Route path="/create">
           <CreateNew addNew={addNew} />
         </Route>
