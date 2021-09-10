770cd4bb513fb35ca6ad6a7ae8b3eae826c70f58
Marcus Gugacs <iimpaq@proton.me>
2021-09-10 21:31:05 +0200

    Exercise 7.4: anecdotes and hooks step1

--- part7/routed-anecdotes/src/App.js
+++ part7/routed-anecdotes/src/App.js
@@ -6,6 +6,7 @@ import {
   useHistory,
   useRouteMatch
 } from "react-router-dom";
+import { useField } from "./hooks/index";
 
 const Notification = ({ message }) => {
   if (message === null) {
@@ -35,7 +36,8 @@ const Anecdote = ({ anecdote }) => {
     <div>
       <h2>{anecdote.content} by {anecdote.author}</h2>
       <p>has {anecdote.votes}</p>
-      <p>for more info see {anecdote.info}</p>
+      <p>for more info see <a href={anecdote.info} target="_blank" rel="noreferrer">
+        {anecdote.info}</a></p>
     </div>
   );
 };
@@ -74,16 +76,16 @@ const Footer = () => (
 );
 
 const CreateNew = (props) => {
-  const [content, setContent] = useState("");
-  const [author, setAuthor] = useState("");
-  const [info, setInfo] = useState("");
+  const content = useField("text");
+  const author = useField("text");
+  const info = useField("text");
 
   const handleSubmit = (e) => {
     e.preventDefault();
     props.addNew({
-      content,
-      author,
-      info,
+      content: content.value,
+      author: author.value,
+      info: info.value,
       votes: 0
     });
   };
@@ -94,15 +96,15 @@ const CreateNew = (props) => {
       <form onSubmit={handleSubmit}>
         <div>
           content
-          <input name="content" value={content} onChange={(e) => setContent(e.target.value)} />
+          <input { ...content } />
         </div>
         <div>
           author
-          <input name="author" value={author} onChange={(e) => setAuthor(e.target.value)} />
+          <input { ...author } />
         </div>
         <div>
           url for more info
-          <input name="info" value={info} onChange={(e) => setInfo(e.target.value)} />
+          <input { ...info } />
         </div>
         <button>create</button>
       </form>
