1ae4f9ec44ed59dd78aae8eeb8a215a31f1149d7
Marcus Gugacs <m.gugacs@protonmail.com>
2021-09-19 20:03:01 +0200

    Exercise 7.15: Blog view

--- part7/bloglist/src/components/Blog.js
+++ part7/bloglist/src/components/Blog.js
@@ -1,13 +1,8 @@
-import React, { useState } from "react";
+import React from "react";
 import PropTypes from "prop-types";
-import {deleteBlog, voteFor} from "../reducers/blogReducer";
-import {notifyWith} from "../reducers/notificationReducer";
-import {useDispatch} from "react-redux";
+import { Link } from "react-router-dom";
 
-const Blog = ({ blog, currentUser }) => {
-  const [showDetails, setShowDetails] = useState(false);
-  const dispatch = useDispatch();
-
+const Blog = ({ blog }) => {
   const blogStyle = {
     paddingTop: 10,
     paddingLeft: 2,
@@ -16,60 +11,15 @@ const Blog = ({ blog, currentUser }) => {
     marginBottom: 5
   };
 
-  const visibleDetails = { display: showDetails ? "" : "none" };
-  const visibleForCreator = { display: currentUser === blog.user.username
-    ? ""
-    : "none" };
-
-  const toggleDetails = (event) => {
-    event.preventDefault();
-    setShowDetails(!showDetails);
-  };
-
-  const handleLike = (event) => {
-    event.preventDefault();
-    try {
-      dispatch(voteFor(blog));
-      dispatch(notifyWith(`you liked '${blog.title}'`, 5));
-    } catch(error) {
-      console.error(error);
-      dispatch(notifyWith("Failed liking blog", 5, true));
-    }
-  };
-
-  const handleDelete = (event) => {
-    event.preventDefault();
-
-    if(window.confirm(`Remove blog ${blog.title} by ${blog.author}?`)) {
-      try {
-        dispatch(deleteBlog(blog.id));
-        dispatch(notifyWith("Deleted blog", 5, false));
-      } catch(error) {
-        console.error(error);
-        dispatch(notifyWith("Failed deleting blog", 5, true));
-      }
-    }
-  };
-
   return (
     <div style={blogStyle}>
-      <div>
-        <strong className="blogTitle">{blog.title}</strong> by {blog.author}
-        <button onClick={toggleDetails} className="buttonView">{showDetails ? "hide" : "view"}</button>
-      </div>
-      <div style={visibleDetails} className="blogDetails">
-        available at {blog.url}<br />
-        likes: {blog.likes} <button onClick={handleLike} className="buttonLike">like</button><br />
-        posted by: {blog.user.name}<br />
-        <button style={visibleForCreator} onClick={handleDelete}>delete</button>
-      </div>
+      <Link to={`/blogs/${blog.id}`}><strong className="blogTitle">{blog.title}</strong> by {blog.author}</Link>
     </div>
   );
 };
 
 Blog.propTypes = {
-  blog: PropTypes.object.isRequired,
-  currentUser: PropTypes.string.isRequired
+  blog: PropTypes.object.isRequired
 };
 
 export default Blog;
