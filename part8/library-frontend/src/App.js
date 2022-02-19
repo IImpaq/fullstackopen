b200309933e93810b3c4d549514cf5d6ed4c0a39
Marcus Gugacs <iimpaq@proton.me>
2022-02-19 20:26:47 +0100

    Exercise 8.24: Subscriptions - client part 1

--- part8/library-frontend/src/App.js
+++ part8/library-frontend/src/App.js
@@ -4,7 +4,8 @@ import Books from "./components/Books";
 import NewBook from "./components/NewBook";
 import LoginForm from "./components/LoginForm";
 import Recommendations from "./components/Recommendations";
-import {useApolloClient} from "@apollo/client";
+import {useApolloClient, useSubscription} from "@apollo/client";
+import {BOOK_ADDED} from "./queries";
 
 const App = () => {
   const [page, setPage] = useState("authors");
@@ -17,6 +18,12 @@ const App = () => {
       setToken(localToken);
     }
   }, []);
+
+  useSubscription(BOOK_ADDED, {
+    onSubscriptionData: ({ subscriptionData }) => {
+      window.alert(subscriptionData);
+    }
+  })
 
   const logout = () => {
     setToken(null);
