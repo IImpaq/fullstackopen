ac16c01d36c927b9341e1de0d4e4b414729ab4aa
Marcus Gugacs <iimpaq@proton.me>
2022-02-21 13:10:09 +0100

    Exercise 9.1: Body mass index

---
+++ part9/first-steps/bmiCalculator.ts
@@ -0,0 +1,14 @@
+const calculateBmi = (height: number, weight: number) : string => {
+  const bmi = weight / ((height / 100) ** 2);
+  console.log(bmi);
+
+  if(bmi <= 18.4) {
+    return "Underweight (unhealthy weight)";
+  } else if(bmi <= 24.9) {
+    return "Normal (healthy weight)";
+  } else {
+    return "Overweight (unhealthy weight)";
+  }
+}
+
+console.log(calculateBmi(180, 74));
